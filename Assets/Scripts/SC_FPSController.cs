322027e23c02bd41a94cfda530f6ac8025c05581
vigneshvicky0501 <vignesharasu0501@gmail.com>
2024-03-05 18:28:49 +0530

    asdfghjkl;

--- Assets/Scripts/SC_FPSController.cs
+++ Assets/Scripts/SC_FPSController.cs
@@ -1,10 +1,11 @@
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 
-public class SC_FPSController1 : MonoBehaviour
+public class SC_FPSController : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
